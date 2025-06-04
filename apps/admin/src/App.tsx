76cffceab11b08577ef3df961fbb66d8670d28e9
kd02109 <kd02109@gmail.com>
2025-06-04 11:41:42 +0900

    feat: carousel 구조 작성

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,6 +1,7 @@
 import '@yaksok/ui/styles.css'
 function App() {
-  const OPTIONS = { dragFree: true, loop: true }
+  const OPTIONS: CarouselOptionsType = { dragFree: true, loop: true }
+  const AUTO_PLAY_OPTIONS: AutoplayOptionsType = { stopOnInteraction: false }
   const SLIDE_COUNT = 5
   const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   return (
