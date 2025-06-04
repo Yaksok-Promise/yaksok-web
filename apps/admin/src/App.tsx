ada9c3b9f9c99ebd0411240569c678b10c9dc83b
kd02109 <kd02109@gmail.com>
2025-06-04 10:03:03 +0900

    fix: tailwindcss 적용 버그 수정

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,8 +1,16 @@
+import Carousel from '@yaksok/ui/carousel/index'
+import '@yaksok/ui/styles.css'
 function App() {
+  const OPTIONS = { dragFree: true, loop: true }
+  const SLIDE_COUNT = 5
+  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   return (
-    <>
+    <div className="h-screen">
       <div>Admin template</div>
-    </>
+      <div className="flex items-center gap-5">
+        <Carousel slides={SLIDES} options={OPTIONS}></Carousel>
+      </div>
+    </div>
   )
 }
 
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Router from './router'
 import '@stackflow/plugin-basic-ui/index.css'
+import '@yaksok/ui/styles.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
