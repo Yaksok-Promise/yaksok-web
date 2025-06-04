56229097399f7625f5ba5fd64262e01494a22862
kd02109 <kd02109@gmail.com>
2025-06-04 13:47:37 +0900

    feat: abort

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,9 +1,5 @@
 import '@yaksok/ui/styles.css'
 function App() {
-  const OPTIONS: CarouselOptionsType = { dragFree: true, loop: true }
-  const AUTO_PLAY_OPTIONS: AutoplayOptionsType = { stopOnInteraction: false }
-  const SLIDE_COUNT = 5
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   return (
     <div className="h-screen">
       <div>Admin template</div>
