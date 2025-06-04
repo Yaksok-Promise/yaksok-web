5133d2e13c08ac605819fb1d0bda6d997a678be0
kd02109 <kd02109@gmail.com>
2025-06-04 16:22:56 +0900

    feat: icon package추가

---
+++ packages/icons/src/Pause.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+const SvgPause = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <path fill="#E3E3E3" d="M10.65 3.6h2.25v10h-2.25zM5.4 3.6h2.25v10H5.4z" />
+  </svg>
+)
+export default SvgPause
