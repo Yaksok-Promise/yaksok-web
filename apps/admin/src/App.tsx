a94d79a41b0589b8f7754e3c3c83ec2cb8746d1f
kd02109 <57277708+kd02109@users.noreply.github.com>
2025-06-06 18:18:47 +0900

    [Feat] Carousel 컴포넌트 작업   (#12)
    * chore: storybook scripts 변경
    
    * feat: carousel animation bar
    
    * feat: icon 추가
    
    * fix: carousel controller css 수정
    
    * feat: carousel dot animtaion components
    
    * feat: carousel 구조 최종 수정
    
    * feat: hook test code 생성
    
    * chore: storybook 빌드
    
    * fix: 불필요한 fragment 제거

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,27 +1,8 @@
-import Carousel, {
-  AutoplayOptionsType,
-  CarouselOptionsType,
-} from '@yaksok/ui/carousel/index'
 import '@yaksok/ui/styles.css'
 function App() {
-  const OPTIONS: CarouselOptionsType = { dragFree: true, loop: true }
-  const AUTO_PLAY_OPTIONS: AutoplayOptionsType = { stopOnInteraction: false }
-  const SLIDE_COUNT = 5
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   return (
     <div className="h-screen">
       <div>Admin template</div>
-      <div className="flex items-center gap-5">
-        <Carousel.Root options={OPTIONS} autoPlayOption={AUTO_PLAY_OPTIONS}>
-          <Carousel.Track>
-            {SLIDES.map(index => (
-              <Carousel.Slide key={index}>{index + 1}</Carousel.Slide>
-            ))}
-          </Carousel.Track>
-          <Carousel.DotButton />
-          <Carousel.Controller />
-        </Carousel.Root>
-      </div>
     </div>
   )
 }
---
+++ packages/ui/src/carousel/carousel-dots.tsx
@@ -0,0 +1,50 @@
+import { cn } from '@yaksok/utils'
+import { EmblaCarouselType } from 'embla-carousel'
+import { useRef } from 'react'
+import { useCarousel } from './carousel-context'
+import { useAutoplayProgress } from './use-auto-play-progress'
+
+interface DotProps {
+  isActive: boolean
+  embla: EmblaCarouselType | undefined
+}
+
+const Dot: React.FC<DotProps> = ({ isActive, embla }) => {
+  const progressRef = useRef<HTMLDivElement>(null)
+  useAutoplayProgress(embla, progressRef)
+
+  const autoPlay = embla?.plugins().autoplay
+  const timeUntilNext = autoPlay?.timeUntilNext()
+
+  if (isActive) {
+    return (
+      <div
+        className={cn(
+          'relative h-1.25 w-[35px] overflow-hidden rounded-[10px] bg-gray06'
+        )}
+      >
+        <div
+          ref={progressRef}
+          className="absolute top-0 bottom-0 w-full origin-left bg-black01"
+          style={{
+            animation: `slide var(--autoplay-duration, ${timeUntilNext}ms) linear forwards`,
+          }}
+        />
+      </div>
+    )
+  }
+
+  return <div className="h-1.25 w-1.25 rounded-full bg-gray06" />
+}
+
+export const CarouselDotButton: React.FC = () => {
+  const { selectedIndex, scrollSnaps, embla } = useCarousel()
+
+  return (
+    <div className="flex flex-wrap items-center gap-2.5">
+      {scrollSnaps.map((_, index) => (
+        <Dot key={index} isActive={selectedIndex === index} embla={embla} />
+      ))}
+    </div>
+  )
+}
