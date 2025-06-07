a2c7d35adb086f18de1ea253fbf9bce1e0220e0d
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-07 18:58:59 +0900

    feat: TextField 컴포넌트 작성 (#7)

--- apps/docs/src/stories/text-field.stories.tsx
+++ apps/docs/src/stories/text-field.stories.tsx
@@ -1,11 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import TextField from '@yaksok/ui/text-field'
-
-type TextFieldProps = React.ComponentProps<'input'>
+import TextField, { TextFieldProps } from '@yaksok/ui/text-field'
 
 const meta: Meta<TextFieldProps> = {
-  title: 'Input/TextField',
+  title: 'stories/text-field',
   component: TextField,
   parameters: {
     layout: 'centered',
@@ -24,20 +22,14 @@ const meta: Meta<TextFieldProps> = {
         disable: true,
       },
     },
-    disabled: {
+    onVerify: {
+      options: [true, false],
       control: {
-        type: 'boolean',
-      },
-    },
-    placeholder: {
-      control: {
-        type: 'text',
+        type: 'select',
       },
     },
   },
-  args: {
-    disabled: false,
-  },
+  args: {},
 } satisfies Meta<TextFieldProps>
 
 export default meta
@@ -45,7 +37,23 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    placeholder: '텍스트 입력',
+    placeholder: '이메일을 입력해 주세요',
     type: 'text',
+    label: '이메일',
+    regex: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    message: {
+      regexError: '이메일 형식이 올바르지 않습니다.',
+      verificationError: '이메일 인증에 실패했습니다.',
+    },
+  },
+  render: args => {
+    const handleVerify = (value: string) => {
+      console.log(value)
+      return args.onVerify as boolean
+    }
+
+    return (
+      <TextField {...({ ...args, onVerify: handleVerify } as TextFieldProps)} />
+    )
   },
 }
--- packages/ui/src/text-field.tsx
+++ packages/ui/src/text-field.tsx
@@ -1,19 +1,102 @@
-import { cn } from '../../utils/src/cn'
+import * as React from 'react'
 
-interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+import { Check } from '@yaksok/icons'
+import { cn } from '@yaksok/utils'
+import { VariantProps, cva } from 'class-variance-authority'
+import { useState } from 'react'
+
+export const textFieldVariants = cva(
+  cn(
+    'w-full py-[14px] text-body2',
+    'bg-gray10 placeholder:text-gray05 focus:outline-none focus:ring-0'
+  )
+)
+
+type ErrorStatus = 'regexError' | 'verificationError'
+type SuccessStatus = 'regexSuccess' | 'success'
+type Status = undefined | ErrorStatus | SuccessStatus
+
+type Message = Partial<Record<ErrorStatus, string>> // 에러에 관한 메세지
+
+export interface TextFieldProps
+  extends React.ComponentProps<'input'>,
+    VariantProps<typeof textFieldVariants> {
+  label: string
+  message: Message
+  regex: RegExp
+  onVerify?: (value: string) => boolean
+}
 
 export default function TextField({
-  className,
   type = 'text',
+  className,
   ...props
 }: TextFieldProps) {
+  const [status, setStatus] = useState<Status>(undefined)
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+
+    if (value.length === 0) {
+      setStatus(undefined)
+      return
+    }
+
+    // 인증 완료 후 입력 시 상태 초기화
+    if (status === 'success') {
+      setStatus(undefined)
+    }
+
+    // 정규식 검사만 필요한 경우
+    if (!props.onVerify) {
+      setStatus(props.regex.test(value) ? 'success' : 'regexError')
+      return
+    }
+
+    //정규식 + 인증 검사 필요한 경우
+    setStatus(props.regex.test(value) ? 'regexSuccess' : 'regexError')
+  }
+
+  const handleVerify = () => {
+    const isVerified = props.onVerify!(props.value as string)
+    setStatus(isVerified ? 'success' : 'verificationError')
+  }
+
   return (
-    <input
-      style={{
-        outline: 'none',
-      }}
-      className={cn('bg-text-body2', className)}
-      {...props}
-    />
+    <div className="flex w-full flex-col">
+      <label className="text-caption1">
+        {status?.includes('Error') ? (
+          <span className="text-red01">
+            {props.message[status as ErrorStatus]}
+          </span>
+        ) : (
+          props.label
+        )}
+      </label>
+      <div className="flex items-center gap-[16px]">
+        <div className="flex w-full flex-1 items-center justify-between border-black01 border-b-[2px]">
+          <input
+            type={type}
+            data-slot="input"
+            className={cn(
+              textFieldVariants({
+                className,
+              })
+            )}
+            onChange={handleInputChange}
+            {...props}
+          />
+          {status === 'success' && <Check />}
+        </div>
+        {props.onVerify && (
+          <button
+            className="h-[25px] w-[66px] rounded-full bg-black01 text-caption1 text-white01 disabled:bg-gray06 disabled:text-gray05"
+            disabled={status === 'regexError' || status === undefined}
+            onClick={handleVerify}
+          >
+            인증하기
+          </button>
+        )}
+      </div>
+    </div>
   )
 }
