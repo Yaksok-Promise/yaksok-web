84148ad415d1b8b3bb7aadace1384675aca79135
kd02109 <kd02109@gmail.com>
2025-06-15 11:19:07 +0900

    fix: stackflow css bug

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,8 +2,8 @@ import { Stack } from '@/utils/stackflow'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Router from './router'
-import '@stackflow/plugin-basic-ui/index.css'
 import '@yaksok/ui/styles.css'
+import '@stackflow/plugin-basic-ui/index.css'
 
 const queryClient = new QueryClient({
   defaultOptions: {
