61b1ce614e54a207f5025861618888ce430f5d8a
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-17 01:50:27 +0900

    feat: 버튼 컴포넌트 작성 (#25)

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,22 +1,55 @@
-import { ReactNode } from 'react'
-import { cn } from '../../utils/src/cn'
+import { cn } from '@yaksok/utils'
+import { type VariantProps, cva } from 'class-variance-authority'
+import * as React from 'react'
 
-interface ButtonProps {
-  children: ReactNode
-  className?: string
-  appName: string
-}
+export type ButtonProps = React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    children: React.ReactNode
+    rounded?: 'full' | 'md'
+    size?: 'full' | 'fit'
+  }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const buttonVariants = cva(
+  'inline-flex py-[12px] text-gray9 font-bold items-center clickable justify-center gap-2 whitespace-nowrap transition-all disabled:pointer-events-none',
+  {
+    variants: {
+      rounded: {
+        full: 'rounded-full',
+        md: 'rounded-[8px]',
+      },
+      variant: {
+        default:
+          'bg-black01 text-subhead1 text-white disabled:bg-gray06 disabled:text-gray05',
+      },
+      size: {
+        full: 'w-full',
+        fit: 'w-fit px-[12px] py-[3.5px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'full',
+      rounded: 'md',
+    },
+  }
+)
+
+function Button({
+  className,
+  variant,
+  size,
+  children,
+  rounded,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className={cn(
-        className,
-        'h-10 w-10 border-[1px] border-red-500 text-[300px]'
-      )}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={cn(buttonVariants({ variant, size, className, rounded }))}
+      {...props}
     >
       {children}
     </button>
   )
 }
+
+export { Button, buttonVariants }
