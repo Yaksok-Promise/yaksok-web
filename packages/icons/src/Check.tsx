cb65573ed41093184dee586aa2725f4608400bfa
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-18 02:58:46 +0900

    design: Check 아이콘 svg 수정 (#39)

--- packages/icons/src/Check.tsx
+++ packages/icons/src/Check.tsx
@@ -2,19 +2,19 @@ import * as React from 'react'
 import type { SVGProps } from 'react'
 const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
     {...props}
   >
     <path
-      d="M6 12L10.2426 16.2426L18.727 7.75732"
-      stroke="black"
-      strokeWidth="1.5"
+      stroke="#000"
       strokeLinecap="round"
       strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m6 12 4.243 4.243 8.484-8.486"
     />
   </svg>
 )
