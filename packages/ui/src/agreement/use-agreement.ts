c298a2482988b93af25e2e90829717b90e664d97
gaeunnlee <gaeunnnleee@gmail.com>
2025-06-18 03:06:04 +0900

    feat: useAgreement 훅 작성 (#39)

---
+++ packages/ui/src/agreement/use-agreement.ts
@@ -0,0 +1,72 @@
+import { Dispatch, useReducer } from 'react'
+import { AgreementItemContent } from '.'
+
+export const useAgreement = (itemList: AgreementItemContent[]) => {
+  const itemIdList = itemList.map(item => item.id)
+  const defaultItemCheckedState = itemIdList.reduce(
+    (acc, id) => ({ ...acc, [id]: false }),
+    {}
+  )
+
+  type ItemId = (typeof itemIdList)[number]
+
+  type State = Record<ItemId, boolean>
+
+  type Action =
+    | { type: 'CHECK_ITEM'; itemId: ItemId }
+    | { type: 'CHECK_ALL' }
+    | { type: 'UNCHECK_ALL' }
+
+  const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+      case 'CHECK_ITEM':
+        return { ...state, [action.itemId]: !state[action.itemId] }
+      case 'CHECK_ALL': // 필수 항목들만 모두 체크
+        return Object.keys(state).reduce(
+          (acc, id) => ({
+            ...acc,
+            [id]:
+              itemList.find(item => item.id === id)?.isRequired || state[id],
+          }),
+          {}
+        )
+      case 'UNCHECK_ALL':
+        return Object.keys(state).reduce(
+          (acc, id) => ({ ...acc, [id]: false }),
+          {}
+        )
+      default:
+        return state
+    }
+  }
+
+  const [itemsChecked, dispatch]: [State, Dispatch<Action>] = useReducer(
+    reducer,
+    defaultItemCheckedState
+  )
+
+  const handleCheckItem = (id: ItemId) => {
+    dispatch({ type: 'CHECK_ITEM', itemId: id })
+  }
+
+  const handleCheckAll = () => {
+    dispatch({ type: 'CHECK_ALL' })
+  }
+
+  const handleUncheckAll = () => {
+    dispatch({ type: 'UNCHECK_ALL' })
+  }
+
+  // 모든 필수 항목들의 체크 여부
+  const isAgreementCheckedComplete: boolean = itemList
+    .filter(item => item.isRequired)
+    .every(item => itemsChecked[item.id])
+
+  return {
+    itemsChecked,
+    handleCheckItem,
+    handleCheckAll,
+    handleUncheckAll,
+    isAgreementCheckedComplete,
+  }
+}
