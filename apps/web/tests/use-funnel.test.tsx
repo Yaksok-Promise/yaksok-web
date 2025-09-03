bbc49ae3e834efc158f9c54cbc9d4bf0e24c6cf0
kd02109 <kd02109@gmail.com>
2025-09-03 16:00:45 +0900

    fix: useFunnel Test code fix

--- apps/web/tests/use-funnel.test.tsx
+++ apps/web/tests/use-funnel.test.tsx
@@ -6,13 +6,19 @@ import React from 'react'
 import { afterEach, describe, expect, it } from 'vitest'
 import { useFunnel } from '../src/hooks/use-funnel'
 
-const TestComponent = ({ defaultStep }: { defaultStep: string }) => {
-  const { Funnel, Step, currentStep, setStep } = useFunnel(defaultStep)
+const TestComponent = ({
+  steps,
+  defaultStep,
+}: { steps: string[]; defaultStep: string }) => {
+  const { Funnel, Step, currentStep, handleNext } = useFunnel<string>(
+    steps,
+    defaultStep
+  )
 
   return (
     <div>
       <div>currentStep: {currentStep}</div>
-      <button onClick={() => setStep('step2')}>Go to Step 2</button>
+      <button onClick={handleNext}>Go to Step 2</button>
       <Funnel>
         <Step name="step1">
           <div>Step 1 Content</div>
@@ -31,14 +37,14 @@ describe('useFunnel', () => {
   })
 
   it('초기 step이 올바르게 설정되어야 한다', () => {
-    render(<TestComponent defaultStep="step1" />)
+    render(<TestComponent defaultStep="step1" steps={['step1', 'step2']} />)
     expect(screen.getByText('currentStep: step1')).toBeInTheDocument()
     expect(screen.getByText('Step 1 Content')).toBeInTheDocument()
     expect(screen.queryByText('Step 2 Content')).not.toBeInTheDocument()
   })
 
   it('setStep을 호출하면 다른 step이 렌더링되어야 한다', () => {
-    render(<TestComponent defaultStep="step1" />)
+    render(<TestComponent defaultStep="step1" steps={['step1', 'step2']} />)
     const button = screen.getByRole('button', { name: 'Go to Step 2' })
     fireEvent.click(button)
 
