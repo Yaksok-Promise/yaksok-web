22895680d662dc55fea91f785ad61dcf8a2b4142
kd02109 <kd02109@gmail.com>
2025-10-01 18:08:39 +0900

    refactor: floating button component화

--- apps/web/src/components/magazine/magazine-floating-button.tsx
+++ apps/web/src/components/magazine/magazine-floating-button.tsx
@@ -3,8 +3,9 @@ import {
   useMagazineScrapCountOptimistic,
 } from '@/hooks/tanstak/use-optimistic-post'
 import { BlankHeart, Bookmark, Share } from '@yaksok/icons'
+import { FloatingButton } from '@yaksok/ui'
 import { cn } from '@yaksok/utils'
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, useMemo } from 'react'
 
 export type MagazineFloatingButtonProps = ComponentPropsWithoutRef<'div'> & {
   inView: boolean
@@ -29,34 +30,33 @@ export const MagazineFloatingButton = ({
   const handleShare = () => {
     console.log('share')
   }
-  return (
-    <div
-      className={cn(
-        'fixed right-7 bottom-16 flex flex-col items-center justify-center gap-1 rounded-2xl bg-gray03/20 px-1.5 py-2',
-        'will-change-[opacity]',
-        'transition-[opacity] duration-300 ease-in-out',
-        !inView ? 'translate-x-0 opacity-100' : 'pointer-events-none opacity-0',
-        className
-      )}
-      {...props}
-    >
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  const buttonList = useMemo(
+    () => [
       <button onClick={handleLike}>
         <BlankHeart
-          size={20}
+          size={24}
           stroke="#636366"
           fill={liked ? '#636366' : 'none'}
         />
-      </button>
+      </button>,
       <button onClick={handleScrap}>
         <Bookmark
           size={24}
           stroke="#636366"
           fill={scraped ? '#636366' : 'none'}
         />
-      </button>
+      </button>,
       <button onClick={handleShare}>
         <Share size={24} stroke="#636366" />
-      </button>
-    </div>
+      </button>,
+    ],
+    [liked, scraped, magazineId]
+  ) as React.ReactHTMLElement<HTMLButtonElement>[]
+  return (
+    <FloatingButton inView={inView} className={className} {...props}>
+      {buttonList}
+    </FloatingButton>
   )
 }
--- packages/icons/src/BlankHeart.tsx
+++ packages/icons/src/BlankHeart.tsx
@@ -1,32 +1,68 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
-
 const SvgBlankHeart = React.forwardRef<
   SVGSVGElement,
   SVGProps<SVGSVGElement> & {
     size?: number | string
-    stroke?: string
-    fill?: string
   }
 >(({ size = 24, stroke = 'black', fill = 'none', ...props }, ref) => (
   <svg
     width={size}
     height={size}
-    viewBox="0 0 18 18"
+    viewBox="0 0 24 24"
+    fill={fill}
     xmlns="http://www.w3.org/2000/svg"
     ref={ref}
     {...props}
   >
     <path
-      d="M9 5.77098C7.5 2.25015 2.25 2.62515 2.25 7.12518C2.25 11.6252 9 15.3753 9 15.3753C9 15.3753 15.75 11.6252 15.75 7.12518C15.75 2.62515 10.5 2.25015 9 5.77098Z"
-      stroke={stroke}
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke="#48484A"
       strokeWidth={1.5}
       strokeLinecap="round"
       strokeLinejoin="round"
-      fill={fill}
+    />
+    <path
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke={stroke}
+      strokeOpacity={0.2}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke={stroke}
+      strokeOpacity={0.2}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke={stroke}
+      strokeOpacity={0.2}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke="black"
+      strokeOpacity={0.2}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12 7.69431C10 2.99988 3 3.49988 3 9.49991C3 15.4999 12 20.5001 12 20.5001C12 20.5001 21 15.4999 21 9.49991C21 3.49988 14 2.99988 12 7.69431Z"
+      stroke={stroke}
+      strokeOpacity={0.2}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 ))
-
 SvgBlankHeart.displayName = 'SvgBlankHeart'
 export default SvgBlankHeart
